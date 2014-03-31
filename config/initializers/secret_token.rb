7e55be2f21a22f44e2fd1bf56aed116dd33658b4
beekermememe <bkeogh123@gmail.com>
2014-03-30 20:32:35 -0600

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beekerwebsite::Application.config.secret_key_base = '80830f8570aca051e6c2699ca9c31204d5e52abd99b4a2e41dc32538b955eed9bf4597affc51b0efe41b7493f37b5deee171fb19049f18f27371eef770e3942a'
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(
+  name: 'beeker',
+  email: 'bkeogh123@gmail.com',
+  password: '\xB3\x94\xB4\xC0\xC65Y\xD8~8\x89\x94\xDB\x93\xA9\x93\xC2\xCC\xA2\x14\xB3Hz\x8C\xD9v)\xAEI\xEB!\x91'
+)
